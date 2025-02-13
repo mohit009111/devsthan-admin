44fb387355b272721ceb579c1ae2feed0230203a
devsthan-expert <indiadevsthan@gmail.com>
2025-02-13 16:42:30 +0530

    latest

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,7 +1,7 @@
 const PORT = 4000
 
 
-const BASE_URL=`http://localhost:${PORT}`
+const BASE_URL=`https://devsthan-backend-1.onrender.com`
 
 const VERSEL_URL="https://devsthan-backend-7lid.onrender.com"
 
